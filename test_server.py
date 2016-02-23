b00de41881b388640aa22e4313a254ef88134183
hessam <hessam@ubuntu.(none)>
2016-02-23 16:45:54 -0600

    v5

--- test_server.py
+++ test_server.py
@@ -139,10 +139,10 @@ class TestServer(unittest.TestCase):
         self.api0.assert_set('a'*100, 'b'*100)
         self.assert_disconnect(self.api0)
 
-    # def test_disconnect_after_get(self):
-    #     self.api0.assert_set('a'*500, 'b'*500)
-    #     self.api0.assert_get('a'*500, 'b'*500)
-    #     self.assert_disconnect(self.api0)
+    def test_disconnect_after_get(self):
+        self.api0.assert_set('a'*500, 'b'*500)
+        self.api0.assert_get('a'*500, 'b'*500)
+        self.assert_disconnect(self.api0)
 
     # def test_example_session(self):
     #     self.api0.assert_set('foo', 'bar')
